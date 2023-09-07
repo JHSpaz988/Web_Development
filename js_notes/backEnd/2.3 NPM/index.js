fbb49211cacd7d5011187ac983a19b4b11959a0a
JHSpaz988 <JHSpaz988@gmail.com>
2023-09-07 02:10:12 -0400

    QR generator project completion, node/npm notes

---
+++ js_notes/backEnd/2.3 NPM/index.js
@@ -0,0 +1,11 @@
+// import generateName from "sillyname";
+
+// const sillyName = generateName();
+
+// console.log(`My name is ${sillyName}.`);
+
+import superheroes from "superheroes";
+
+const superHeroName = superheroes.random();
+
+console.log(`My superhero name is ${superHeroName}!`);
---
+++ js_notes/backEnd/2.4 QR Code Project/index.js
@@ -0,0 +1,27 @@
+import fs from "fs";
+import inquirer from "inquirer";
+import qr from "qr-image";
+inquirer
+  .prompt([
+    {
+      message: "Enter a url to turn into a QR code.",
+      name: "url",
+    },
+  ])
+  .then((answers) => {
+    const url = answers.url;
+    const qrCode = qr.image(url);
+    qrCode.pipe(fs.createWriteStream("QR.png"));
+
+    fs.writeFile("url.text", url, (err) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+    });
+  })
+  .catch((error) => {
+    if (error.isTtyError) {
+      console.log("Promp couldn't be rendered in the current environment");
+    }
+  });
