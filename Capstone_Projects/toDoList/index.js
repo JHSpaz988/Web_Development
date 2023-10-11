dd3ad23f80394a59b7b3085f0bacbe07341cb6a1
JHSpaz988 <JHSpaz988@gmail.com>
2023-10-11 04:27:51 -0400

    update toDoApp to add mongoose and DB functions

--- Capstone_Projects/toDoList/index.js
+++ Capstone_Projects/toDoList/index.js
@@ -1,4 +1,5 @@
 import express, { urlencoded } from "express";
+import mongoose from "mongoose";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -8,48 +9,145 @@ const post = 3000;
 
 let referer = "";
 
-let toDoList = [];
 let listHeader = "";
 
 app.use(urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
+mongoose.connect("mongodb://localhost:27017/todolistDB");
+
+const itemsSchema = new mongoose.Schema({
+  name: String,
+});
+
+const Item = mongoose.model("Item", itemsSchema);
+
+const todo1 = new Item({
+  name: "Welcome to your todo list",
+});
+const todo2 = new Item({
+  name: "Hit the + button to add a new item",
+});
+const todo3 = new Item({
+  name: "<-- Click this to mark items completed and remove them from list",
+});
+
+const defaultItems = [todo1, todo2, todo3];
+
+const listSchema = {
+  name: String,
+  items: [itemsSchema],
+};
+
+const List = mongoose.model("List", listSchema);
+
+let navBarList = List.distinct("name").exec();
+
+// Home route
+app.get("/", async (req, res) => {
   referer = req.url;
   listHeader = "All tasks:";
 
-  res.render("index.ejs", {
-    toDoList: toDoList,
-    listHeader: listHeader,
-    referer: referer,
-  });
+  const toDoList = await Item.find({});
+
+  if (toDoList.length === 0) {
+    await Item.insertMany(defaultItems);
+
+    res.redirect(referer);
+  } else {
+    res.render("index.ejs", {
+      toDoList: toDoList,
+      listHeader: listHeader,
+      navBarList: navBarList,
+    });
+  }
 });
 
-app.post("/submit", (req, res) => {
-  let newToDo = req.body.newToDo;
-  toDoList.push(newToDo);
+// Submit Items route
+app.post("/submit", async (req, res) => {
+  const newToDo = new Item({
+    name: req.body.newToDo,
+  });
 
+  if (referer === "/") {
+    try {
+      await newToDo.save();
+
+      console.log("Item added successfully.");
+    } catch (err) {
+      console.log("Error while adding item:", err);
+      res.status(500).send("Internal Server Error");
+    }
+  } else {
+    try {
+      await List.findOneAndUpdate(
+        { name: referer.substring(1) },
+        { $push: { items: newToDo } }
+      );
+      console.log("Item added successfully.");
+    } catch (err) {
+      console.log("Error while adding item:", err);
+      res.status(500).send("Internal Server Error");
+    }
+  }
   res.redirect(referer);
 });
 
-app.get("/work", (req, res) => {
-  referer = req.url;
-  listHeader = "Work related task:";
-
-  res.render("index.ejs", {
-    listHeader: listHeader,
-    referer: referer,
-  });
+// Delete Items route
+app.post("/delete", async (req, res) => {
+  if (referer === "/") {
+    try {
+      if (req.body.checkbox) {
+        const result = await Item.deleteOne({ _id: req.body.itemId });
+      }
+    } catch (err) {
+      console.error("Error while deleting item:", err);
+      res.status(500).send("Internal Server Error");
+    }
+  } else {
+    try {
+      if (req.body.checkbox) {
+        const result = await List.findOneAndUpdate(
+          { name: referer.substring(1) },
+          { $pull: { items: { _id: req.body.itemId } } }
+        );
+        console.log(result);
+      }
+    } catch (err) {
+      console.error("Error while deleting item:", err);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+  res.redirect(referer);
 });
 
-app.get("/recent", (req, res) => {
+app.get("/:customListName", async (req, res) => {
   referer = req.url;
-  listHeader = "Today:";
+  const customListName = req.params.customListName;
 
-  res.render("index.ejs", {
-    listHeader: listHeader,
-    referer: referer,
-  });
+  try {
+    const result = await (async () => {
+      let existingList = await List.findOne({ name: customListName });
+      if (!existingList) {
+        const list = new List({
+          name: customListName,
+          items: defaultItems,
+        });
+
+        await list.save();
+        return list;
+      }
+      return existingList;
+    })();
+    res.render("index.ejs", {
+      toDoList: result.items,
+      listHeader: result.name,
+      navBarList: await navBarList,
+    });
+  } catch (err) {
+    console.error("Error while searching for list");
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 app.listen(post, () => {
